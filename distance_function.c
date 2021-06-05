a574771584bc9357494102e47a484423a15944f4
nourr-aziz <84997629+nourr-aziz@users.noreply.github.com>
2021-06-06 00:32:52 +0200

    Update distance_function.c

--- distance_function.c
+++ distance_function.c
@@ -9,6 +9,18 @@ unsigned char distance_m(unsigned char latitude1, unsigned char longitude1, unsi
     unsigned char longitude1_rad = longitude1*(PI/180);
     unsigned char latitude2_rad = latitude2*(PI/180);
     unsigned char longitude2_rad = longitude2*(PI/180);
-     
-    ghxuyfjg
+    
+    // Haversine Formula
+    double dlongitude = longitude2_rad - longitude1_rad;
+    double dlatitude = latitude2_rad - latitude1_rad;
+    double ans = pow(sin(dlatitude / 2), 2) +  cos(latitude1) * cos(latitude2) * pow(sin(dlongitude / 2), 2);
+ 
+  ans = 2 * asin(sqrt(ans));
+	
+    // Radius of Earth in Kilometers, R = 6371   
+  
+    return (ans * R)/1000;
+}
+
+
         
